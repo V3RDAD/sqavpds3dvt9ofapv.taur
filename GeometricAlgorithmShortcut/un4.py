85028b79a4044e1976e03173437c001808b765d2
V3RDAD <50222317+V3RDAD@users.noreply.github.com>
2022-06-03 12:30:28 +0200

    Create un4.py

---
+++ GeometricAlgorithmShortcut/un4.py
@@ -0,0 +1,8 @@
+import random
+import decimal
+while True:
+   x = decimal.Decimal(random.randrange(1,10))/decimal.Decimal(random.randrange(1,10))
+   y = x * x
+   z = y * x
+   w = x * y * z
+   print(w)
